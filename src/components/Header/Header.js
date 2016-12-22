cd5803c679597cbf19d3774b88031c1022430609
Pavel Svitek <svitek.pavel@gmail.com>
2016-12-22 15:26:37 +0100

    added link to Swapi on homepage

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -12,6 +12,10 @@ export const Header = () => (
     <Link to='/counter' activeClassName='route--active'>
       Counter
     </Link>
+    {' · '}
+    <Link to='/swapi' activeClassName='route--active'>
+      SWAPI
+    </Link>
   </div>
 )
 
