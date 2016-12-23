fb0b519636be580723b0a0ac9087b88ef1bc1fe7
Pavel Svitek <svitek.pavel@gmail.com>
2016-12-23 11:25:40 +0100

    added ability to go to 'next' and 'prev' to swapi listPeople action creator

--- src/routes/Swapi/modules/swapi.js
+++ src/routes/Swapi/modules/swapi.js
@@ -4,11 +4,13 @@ const fetchApi = require('../../../lib/fetch-api').fetchApi
 // ------------------------------------
 export const LIST_PEOPLE = 'LIST_PEOPLE'
 export const LIST_SPACESHIPS = 'LIST_SPACESHIPS'
+export const SET_PEOPLE_NAV_URLS = 'SET_PEOPLE_NAV_URLS'
+export const SET_SPACESHIPS_NAV_URLS = 'SET_SPACESHIPS_NAV_URLS'
 
 // URLs
-const API_BASE = 'http://swapi.co/api'
-const API_PEOPLE = `${API_BASE}/people/`
-const API_SPACESHIPS = `${API_BASE}/starships/`
+export const API_BASE = 'http://swapi.co/api'
+export const API_PEOPLE = `${API_BASE}/people/`
+export const API_SPACESHIPS = `${API_BASE}/starships/`
 
 // ------------------------------------
 // Actions
@@ -18,27 +20,75 @@ const API_SPACESHIPS = `${API_BASE}/starships/`
     returns a function for lazy evaluation. It is incredibly useful for
     creating async actions, especially when combined with redux-thunk! */
 
-export const listPeople = () => {
+export const listPeople = (page) => {
   return (dispatch, getState) => {
-    return fetchApi(API_PEOPLE)
+    const state = getState()
+    let url
+
+    if (page === 'next') {
+      url = state.swapi.nav.people.next
+    } else if (page === 'prev') {
+      url = state.swapi.nav.people.prev
+    } else {
+      url = API_PEOPLE
+    }
+
+    if (url === null) {
+      return Promise.resolve()
+    }
+
+    return fetchApi(url)
       .then((data) => {
         console.log(data);
+
         dispatch({
           type: LIST_PEOPLE,
           payload: data.results,
+        })
+
+        dispatch({
+          type: SET_PEOPLE_NAV_URLS,
+          payload: {
+            next: data.next,
+            prev: data.previous,
+          },
         })
       })
   }
 }
 
-export const listSpaceships = () => {
+export const listSpaceships = (page) => {
   return (dispatch, getState) => {
-    return fetchApi(API_SPACESHIPS)
+    const state = getState()
+    let url
+
+    if (page === 'next') {
+      url = state.swapi.nav.spaceships.next
+    } else if (page === 'prev') {
+      url = state.swapi.nav.spaceships.prev
+    } else {
+      url = API_SPACESHIPS
+    }
+
+    if (url === null) {
+      return Promise.resolve()
+    }
+
+    return fetchApi(url)
       .then((data) => {
         console.log(data);
+
         dispatch({
           type: LIST_SPACESHIPS,
           payload: data.results,
+        })
+
+        dispatch({
+          type: SET_SPACESHIPS_NAV_URLS,
+          payload: {
+            next: data.next,
+            prev: data.previous,
+          },
         })
       })
   }
@@ -55,6 +105,8 @@ export const actions = {
 const ACTION_HANDLERS = {
   [LIST_PEOPLE]     : (state, action) => Object.assign({}, state, {people: action.payload}),
   [LIST_SPACESHIPS] : (state, action) => Object.assign({}, state, {spaceships: action.payload}),
+  [SET_PEOPLE_NAV_URLS]   : (state, action) => Object.assign({}, state, {nav: Object.assign({}, state.nav, {people: action.payload})}),
+  [SET_SPACESHIPS_NAV_URLS]   : (state, action) => Object.assign({}, state, {nav: Object.assign({}, state.nav, {spaceships: action.payload})}),
 }
 
 // ------------------------------------
@@ -63,6 +115,16 @@ const ACTION_HANDLERS = {
 const initialState = () => ({
   people: [],
   spaceships: [],
+  nav: {
+    people: {
+      next: API_PEOPLE,
+      previous: null,
+    },
+    spaceships: {
+      next: API_SPACESHIPS,
+      previous: null,
+    },
+  }
 })
 
 export default function reducer (state = initialState(), action) {
